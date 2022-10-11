22ba6692d85184fa8b29e86bb76477fe5fa9fd2f
M45Hasan <mmhasan045@gmail.com>
2022-10-11 08:05:09 +0600

    added

--- js/script.js
+++ js/script.js
@@ -11,12 +11,27 @@ function myFunction() {
     let arr2 = Array.from(x);
     for (let itm = 0; itm < x.length; itm++) {
       let a = x[itm];
-      
+
       if (a.innerHTML.toUpperCase().search(filter) > -1) {
         a.style.display = "";
       } else {
         a.style.display = "none";
       }
+    }
+  });
+
+  let p = document.querySelectorAll("P");
+  let arr3 = Array.from(p);
+  arr3.map((im) => {
+    let z = im.innerHTML.toUpperCase();
+    console.log(im);
+
+    if (z.match(filter)) {
+      document.getElementById("demo").innerHTML = z.match(filter);
+
+      im.style.color = "yellow";
+    } else {
+      im.style.color = "red";
     }
   });
 }
